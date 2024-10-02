c4a402eb80356e2d679896f423b53132e45986d7
T0mas Sousa <tomasrafael@ua.pt>
2024-10-02 15:51:32 +0100

    Refactor jest.config.js to use 'jest-environment-jsdom' for test environment

--- aula_02-10/jest.config.js
+++ aula_02-10/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[tj]s?(x)"
